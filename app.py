22c1d79823eb3a151eac1915f2dcef47e6548ff3
Adrián Kamycki <adrian.kamycki@mongodb.com>
2024-02-16 21:09:14 +0100

    Revert "Revert "Revert "Add logging middleware to log HTTP request/response"""
    This reverts commit 98a8e87dc6048f74d1286c61544c56b2617457b3.

--- app.py
+++ app.py
@@ -1,21 +1,4 @@
-from flask import Flask, request
-
-import pprint
-
-class LoggingMiddleware(object):
-    def __init__(self, app):
-        self._app = app
-
-    def __call__(self, env, resp):
-        errorlog = env['wsgi.errors']
-        pprint.pprint(('REQUEST', env), stream=errorlog)
-
-        def log_response(status, headers, *args):
-            pprint.pprint(('RESPONSE', status, headers), stream=errorlog)
-            return resp(status, headers, *args)
-
-        return self._app(env, log_response)
-
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
@@ -23,5 +6,4 @@ def index():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.wsgi_app = LoggingMiddleware(app.wsgi_app)
     app.run(host='0.0.0.0', port=8080)
