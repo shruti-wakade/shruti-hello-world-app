b3ad150ab1267c7c32569ec8c28ded3f3e92cd4f
Adrián Kamycki <adrian.kamycki@mongodb.com>
2024-02-16 20:01:16 +0100

    Add logging middleware to log HTTP request/response

--- app.py
+++ app.py
@@ -1,4 +1,21 @@
-from flask import Flask
+from flask import Flask, request
+
+import pprint
+
+class LoggingMiddleware(object):
+    def __init__(self, app):
+        self._app = app
+
+    def __call__(self, env, resp):
+        errorlog = env['wsgi.errors']
+        pprint.pprint(('REQUEST', env), stream=errorlog)
+
+        def log_response(status, headers, *args):
+            pprint.pprint(('RESPONSE', status, headers), stream=errorlog)
+            return resp(status, headers, *args)
+
+        return self._app(env, log_response)
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,4 +23,5 @@ def index():
     return 'Hello, World!'
 
 if __name__ == '__main__':
+    app.wsgi_app = LoggingMiddleware(app.wsgi_app)
     app.run(host='0.0.0.0', port=8080)
